55f7b03d7b16464661acc7d19f658721fb7400a6
Valariael <axel.ledermann@edu.univ-fcomte.fr>
2018-12-05 23:10:20 +0100

    P2PFunction : ajout de getLocalFiles

--- src/comServCli/P2PFunctions.java
+++ src/comServCli/P2PFunctions.java
@@ -1,5 +1,6 @@
 package comServCli;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /* LPRO 2018/2019
@@ -8,7 +9,8 @@ import java.util.ArrayList;
 0  and open the template in the editor.
 0 */
 /**
- *
+ * Classe contenant les fonctions de traitement pour le client et le serveur.
+ * 
  * @author Axel Couturier
  */
 public class P2PFunctions {
@@ -39,5 +41,28 @@ public class P2PFunctions {
             System.out.println("Il n'y a aucun résultat");
         }
 
+    }
+    
+    /**
+     * Recherche tous les fichiers présents dans le dossier passé en paramètre.
+     * 
+     * @param folder le dossier cible
+     * @return la liste des P2PFile créée à partir des fichiers trouvés dans le dossier
+     */
+    public static ArrayList<P2PFile> getLocalFiles(File folder) {
+        ArrayList<P2PFile> fileList = new ArrayList<>();
+        
+        for (final File fileEntry : folder.listFiles()) {
+            // On n'affiche pas les dossiers
+            if (!fileEntry.isDirectory()) {
+                // Pour chaque fichier, on créé un P2PFile avec son nom et sa taille et on l'ajoute a la liste.
+                // TODO : Possiblité d'optimisation : deux new file
+                fileList.add(new P2PFile(folder.getAbsolutePath() + "\\" + fileEntry.getName(), fileEntry.length()));
+                System.out.println("DEBUG : (localfile) " + folder.getAbsolutePath() + "\\" + fileEntry.getName());
+            }
+        }
+        // TODO: possibilité d'évolution = recherche récursive des fichiers
+        
+        return fileList;
     }
 }
